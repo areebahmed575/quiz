3a7e489e13bd14e94be1155bd5da485f2a7766c1
areebahmed575 <areebahmed575@gmail.com>
2023-02-14 02:03:00 +0500

    add quizProject

---
+++ quiz.ts
@@ -0,0 +1,49 @@
+import inquirer from "inquirer";
+type ansType={
+    firstQuestion:string,
+    secondQuestion:string,
+    thirdQuestion:string,
+}
+let answer :ansType= await inquirer.prompt([
+    {
+        type:"list",
+        name:"firstQuestion",
+        choices:["cricket","basketball","football","hockey"],
+        message:"Which is national sport of Pakistan"
+
+    },
+    {
+        type:"list",
+        name:"secondQuestion",
+        choices:["Karachi","Lahore","Queta","Islamabad"],
+        message:"Which is largest city of Pakistan"
+
+
+    },
+    {
+        type:"list",
+        name:"thirdQuestion",
+        choices:["Rose","Jasmine","Sunflower","Tulip"],
+        message:"Which is national flower of Pakistan"
+
+
+    },
+
+])
+if(answer.firstQuestion){
+    let result="hockey"
+    let score=0
+    if(answer.firstQuestion===result){
+        score= score +5
+    }
+    result ="Karachi"
+    if(answer.secondQuestion===result){
+        score= score +5
+        
+    }
+    result="Jasmine"
+    if(answer.thirdQuestion===result){
+        score= score +5
+    }
+    console.log("Your score is "+score+"/15 ")
+}
